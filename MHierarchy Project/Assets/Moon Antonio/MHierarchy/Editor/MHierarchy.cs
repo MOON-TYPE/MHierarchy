58ada989f623a11cdd73b0420cb0778d236a204e
Moon Antonio <pinchodev91@gmail.com>
2017-05-30 16:46:34 +0200

    Update MHierarchy.cs

---
+++ MHierarchy Project/Assets/Moon Antonio/MHierarchy/Editor/MHierarchy.cs
@@ -0,0 +1,24 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// MHierarchy.cs (30/05/2017)													\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:		Editor de la jerarquia de Unity3D							\\
+// Fecha Mod:		30/05/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio.MHierarchy
+{
+	/// <summary>
+	/// <para>Editor de la jerarquia de Unity3D</para>
+	/// </summary>
+	[AddComponentMenu("MoonAntonio/MHierarchy/MHierarchy")]
+	public class MHierarchy : MonoBehaviour 
+	{
+
+	}
+}
